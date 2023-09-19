2b0a62b5fb479b28e605959ee70bec630450650a
Syed Rabeet <92630941+Rabeet8@users.noreply.github.com>
2023-09-19 10:42:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import hashlib
 
 class Block:
-    def __init__(self, data, prev_hash):
+    def __init__(self, data, previous_hash):
         self.data = data
-        self.prev_hash = prev_hash
-        self.hash = self.calc_hash()
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
 
-    def calc_hash(self):
+    def calculate_hash(self):
         # Create a SHA-256 hash of the block's data and previous hash
         sha = hashlib.sha256()
         sha.update(self.data.encode('utf-8'))
-        sha.update(self.prev_hash.encode('utf-8'))
+        sha.update(self.previous_hash.encode('utf-8'))
         return sha.hexdigest()
 
 class Blockchain:
@@ -22,23 +22,23 @@ class Blockchain:
         return Block("Genesis Block", "0")
 
     def add_block(self, data):
-        prev_block = self.chain[-1]
-        new_block = Block(data, prev_block.hash)
+        previous_block = self.chain[-1]
+        new_block = Block(data, previous_block.hash)
         self.chain.append(new_block)
 
 # Create a Blockchain instance
 blockchain = Blockchain()
 
 # Add some blocks to the blockchain
-blockchain.add_block("first block")
-blockchain.add_block("second block")
-blockchain.add_block("third block")
-blockchain.add_block("fourth block")
+blockchain.add_block("First Block")
+blockchain.add_block("Second Block")
+blockchain.add_block("Third Block")
+blockchain.add_block("Fourth Block")
 
 # Print the contents of the blockchain
 print("Blockchain:")
 for block in blockchain.chain:
     print('Data:', block.data)
-    print('Previous hash:', block.prev_hash)
+    print('Previous Hash:', block.previous_hash)
     print('Hash:', block.hash)
     print()
